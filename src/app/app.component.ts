c0c1ef95f9dfe4677ac4e8ea9b28c081d9744607
dhruvkant <dhruvkant1@gmail.com>
2021-02-06 12:04:42 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from "@angular/core";
+import { CupertinoPane, CupertinoSettings } from "cupertino-pane";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  name = "Angular " + VERSION.major;
+
+  public carpentinoPane: CupertinoPane;
+
+  ngOnInit(): void {
+    this.carpentinoPane = new CupertinoPane(".cupertino-pane", {
+      parentElement: "body",
+      breaks: {
+        middle: { enabled: true, height: 200, bounce: true },
+        bottom: { enabled: true, height: 80 }
+      }
+    });
+    this.carpentinoPane.present({ animate: true });
+  }
 }
